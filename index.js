be49214be1ec4748fec2473df7b1afb1de6c8e84
风少 <fengshao@fengshaodeMacBook-Pro.local>
2020-09-25 13:10:35 +0800

    添加log

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 app.use(require('prerender-node'));
+app.use('*', (req, res, next) => {
+    console.log('express');
+    next()
+})
 app.listen(3001);
