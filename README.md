98fcae1a75b5cff7ef22e0d90118ad24b559922b
风少 <fengshao@fengshaodeMacBook-Pro.local>
2020-09-25 15:10:51 +0800

    增加readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Reptiles
+
+## [参考文档](https://juejin.im/post/6844903789661519886)
+## 测试方法
+``` linux
+curl http://localhost:3001/render\?url\=http://62.234.27.149/
+```
--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const server = prerender({
     port: 3001
 });
 server.start();
+// 参考文档：https://juejin.im/post/6844903789661519886
 // prerender测试方法
-// curl http://localhost:3000/render\?url\=http://62.234.27.149/
+// curl http://localhost:3001/render\?url\=http://62.234.27.149/
