74074cf2d4deee88373ae3b885dcc2ad5d1f9c8c
风少 <fengshao@fengshaodeMacBook-Pro.local>
2020-10-10 14:46:17 +0800

    增加中间件

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Reptiles
-
+## 注意
+* 确保已经安装了chrome浏览器
 ## [参考文档](https://juejin.im/post/6844903789661519886)
+
 ## 测试方法
 ``` linux
 curl http://localhost:3001/render\?url\=http://62.234.27.149/
--- index.js
+++ index.js
@@ -6,3 +6,30 @@ server.start();
 // 参考文档：https://juejin.im/post/6844903789661519886
 // prerender测试方法
 // curl http://localhost:3001/render\?url\=http://62.234.27.149/
+
+// 插件方式
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const history = require('connect-history-api-fallback');
+
+const app = express();
+
+app.use(require('prerender-node')
+.set('prerenderServiceUrl', 'http://localhost:3001/')
+);
+const staticFileMiddleware = express.static('dist');
+app.use(staticFileMiddleware);
+// app.use(history({
+//     index: '/dist/index.html'
+// }));
+// app.use(staticFileMiddleware);
+app.get('*', function(req, res)  {
+    res.setHeader('Content-Type', 'text/html')
+    // res.send(fs.readFileSync(path.join(__dirname,'./dist/index.html')))
+    res.sendFile(`${__dirname}/dist/index.html`)
+})
+app.listen(3002)
+
+// curl -H 'User-agent:Googlebot' http://localhost:3002/http://localhost:3001
+
